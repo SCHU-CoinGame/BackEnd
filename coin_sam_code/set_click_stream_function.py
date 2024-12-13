fe4458a13a9e73fe936231733cb62354fd41bde9
yih5025 <yih5025@naver.com>
2024-12-14 06:03:18 +0900

    adding click stream kinesis

---
+++ coin_sam_code/set_click_stream_function.py
@@ -0,0 +1,29 @@
+import json
+import boto3
+import os
+
+kinesis_client =  boto3.client('kinesis', reigion_name = os.environ['AWS_REGION'])
+
+KINESIS_STREAM_NAME = os.environ['KINESIS_STREAM_NAME']
+
+def set_click_stream_handler(event, context):
+    try:
+        body = json.loads(event['body'])
+
+        response = kinesis_client.put_record(
+            StreamName = KINESIS_STREAM_NAME,
+            Data = json.dumps(body),
+            PartitionKey = "~"
+        )
+        print(f"Data send to Kinesis, Response: {response}")
+
+        return {
+            'statusCode': 200,
+            'body': json.dumps({'message': 'Data sent Successfully'})
+        }
+    except Exception as e:
+        print(f"Error: {str(e)}")
+        return {
+            'statusCode': 500,
+            'body': json.dumps({'error': str(e)})
+        }
